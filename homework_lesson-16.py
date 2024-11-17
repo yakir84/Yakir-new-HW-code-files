946ee66ca932f2513c4006fed4e9a5bd878d352f
Yakir zuberi <yakir7784@gmail.com>
2024-11-17 14:43:25 +0200

    Update new list and treatment times

--- homework_lesson-16.py
+++ homework_lesson-16.py
@@ -4,11 +4,12 @@ import random
 import requests
 
 
-def downloader(thread_id,text,url):
+def downloader(thread_id,text,url,returns):
     sleep_seconds = random.randint(0, 15)
     time.sleep(sleep_seconds)
     print("Thread " + str(thread_id) + ": " + text + " slept " + str(sleep_seconds) + " seconds" )
     requests.get(url).json()
+    returns.append(thread_id)
 
 def main():
     urls = [
@@ -20,13 +21,15 @@ def main():
         'https://jsonplaceholder.typicode.com/users'
     ]
     threads = []
+    threads_returns = []
     for i, url in enumerate(urls):
-        thread = threading.Thread(target=downloader, args=(i, "I", url))
+        thread = threading.Thread(target=downloader, args=(i, "I", url, threads_returns, ))
         thread.start()
         threads.append(thread)
 
     for thread in threads:
         thread.join()
+    print(threads_returns)
 
 if __name__ == "__main__":
     start = time.time()
